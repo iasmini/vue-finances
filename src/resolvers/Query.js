b5c7b104f68252bb44a51cb03b8cee67f3c50361
iasmini <iasmini@silexsistemas.com.br>
2021-02-04 21:45:31 -0300

    criada query accounts

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,21 @@
 const { getUserId } = require('./../utils')
 
+function accounts(parent, args, ctx, info){
+  const userId = getUserId(ctx)
+  return ctx.db.query.accounts({
+    where: {
+      OR: [
+        {user: {id: userId}},
+        {user: null}
+      ]
+    },
+    orderBy: 'name_ASC'
+  }, info)
+}
+
 function user(parent, args, ctx, info){
   const userId = getUserId(ctx)
   return ctx.db.query.user({where: {id: userId}}, info)
 }
 
-module.exports = { user }
+module.exports = { accounts, user }
