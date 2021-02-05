c3ccea718395f9b55e227641d2cfb45f72908927
iasmini <iasmini@silexsistemas.com.br>
2021-02-04 22:03:08 -0300

    criada query para listar categorias

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,9 +13,32 @@ function accounts(parent, args, ctx, info){
   }, info)
 }
 
+function categories(parent, {kind}, ctx, info){
+  const userId = getUserId(ctx)
+  let AND = [
+    {
+      OR: [
+        {user: {id: userId}},
+        {user: null}
+      ]
+    }
+    ]
+
+  // se filtra por kind, adiciona kind nos parametros
+  AND = !kind ? AND : [...AND, {kind: kind}]
+
+  return ctx.db.query.categories({
+    where: {AND},
+    orderBy: 'name_ASC'
+  }, info)
+}
+
 function user(parent, args, ctx, info){
   const userId = getUserId(ctx)
   return ctx.db.query.user({where: {id: userId}}, info)
 }
 
-module.exports = { accounts, user }
+module.exports = {
+  accounts,
+  categories,
+  user }
