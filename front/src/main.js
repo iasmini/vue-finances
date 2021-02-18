1e8a7c138900d9f6f90120053be4f7c4fe4a143f
iasmini <iasmini@silexsistemas.com.br>
2021-02-18 13:55:48 -0300

    redireciona para tela de login em caso de erro 'jwt malformed', 'jwt expired', 'jwt not active', 'invalid token'

--- front/src/main.js
+++ front/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
-import '@/plugins/vuelidate'
 import vuetify from '@/plugins/vuetify'
 
+import { errorHandler } from '@/utils'
+import '@/plugins/vuelidate'
+
 Vue.config.productionTip = false
+Vue.config.errorHandler = errorHandler
 
 new Vue({
   router,
--- front/src/utils.js
+++ front/src/utils.js
@@ -1,6 +1,17 @@
+const errorHandler = (error, vm, info) => {
+  console.log('Vue [error hanler]: ', error, info)
+  const jwtErrors = ['jwt malformed', 'jwt expired', 'jwt not active', 'invalid token']
+  if (jwtErrors.some(jwtError => error.message.includes(jwtError))) {
+    vm.$router.push({
+      'path': '/signin',
+      query: { redirect: vm.$route.path }
+    })
+  }
+}
+
 const formatError = message => {
   const messageSplit = message.split(':')
   return messageSplit[messageSplit.length - 1].trim()
 }
 
-export { formatError }
+export { errorHandler, formatError }
